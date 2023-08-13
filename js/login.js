aeea6aea22d07fd3c4cae6ec89a76c16fdc1f0a1
JeongMinE2 <130267954+JeongMinE2@users.noreply.github.com>
2023-08-13 16:05:32 +0900

    Login

---
+++ js/login.js
@@ -0,0 +1,24 @@
+// 로그인 버튼 클릭 시 비밀번호 불일치 이벤트 발생
+const login = document.getElementById("login");
+const login_false_box = document.getElementById("login_false_box");
+
+login.addEventListener("click", function(e) {
+    e.preventDefault();
+    login_false_box.style.display = "block";
+})
+
+// 확인 버튼 클릭 시 창 닫기
+let check = document.querySelectorAll(".check");
+
+check.forEach((Element) => {
+    Element.addEventListener("click", function(e) {
+        e.preventDefault();
+        login_false_box.style.display = "none";
+    })
+})
+
+const join = document.querySelector(".join");
+
+join.addEventListener("click", function() {
+    window.location.href = "https://jeongmine2.github.io/Join/";
+})
